1c9c0d77d8662266bf14bf83b6cdfc7d18a56317
alexn <alexandrarangel325@gmail.com>
2024-03-25 00:50:39 -0500

    Programa para registro de visitante.

---
+++ src/Visitante.java
@@ -0,0 +1,27 @@
+public class Visitante {
+
+    public static void main(String[]args) {
+
+        String nombre;
+        int edad;
+        char genero;
+        long telefono;
+        byte numeroApartamento;
+        boolean primeraVisita;
+
+        nombre = "María Garcia Lopez";
+        edad = 34;
+        genero = 'M';
+        telefono = 65122489;
+        numeroApartamento = 23;
+        primeraVisita = true;
+
+        System.out.println("nombre: " + nombre);
+        System.out.println("edad: " + edad);
+        System.out.println("genero: " + genero);
+        System.out.println("telefono: " + telefono);
+        System.out.println("numeroApartamento: " + numeroApartamento);
+        System.out.println("¿primeraVisita?: " + primeraVisita);
+
+    }
+}
